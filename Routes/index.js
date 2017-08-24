30cdb766c45c8369377e7eb4b51e69f2343cd697
Roman <roman@romans-mbp.lan>
2017-08-23 20:49:36 -0400

    Mystery Word works entirely

--- Routes/index.js
+++ Routes/index.js
@@ -34,10 +34,12 @@ router.get("/", function(req, res) {
     res.render('start_game', {newWord: newWord});
   } else { // if there isnt a game session yet, then create one and render the page.
     // req.session.game = game;
+    newWord = [];
+    guessArray = [];
     remaining = {remainingGuesses: 8}
     hiddenWord = words[Math.floor(Math.random() * words.length)]
     // console.log(hiddenWord);
-    res.render('start_game', {newWord: newWord});
+
 
     for (var i = 0; i < hiddenWord.length; i++) {
       let singleLetter = { letter: hiddenWord[i],
@@ -48,8 +50,9 @@ router.get("/", function(req, res) {
   }
   newWord.push(remaining);
   req.session.newWord = newWord;
-  req.session.token = "sdte46tyhft6"
+  req.session.token = "USELESSTOKEN"
   console.log(newWord);
+  res.render('start_game', {newWord: newWord});
 }
 });
 
@@ -60,20 +63,29 @@ router.post("/guess", function(req,res) {
   index = newWord.findIndex(x => x.remainingGuesses);//got this on google, it gets the index of a word
 
   if (hiddenWord.includes(guess)) {
+    let allGuessed = true;
     newWord.forEach(function(single) {
       if (single.letter === req.body.guess) {
         single.guessed = true;
-        res.render("start_game", {newWord: newWord});
+      }
+      if (single.guessed === false) {
+        allGuessed = false;
       }
     })
+    if (allGuessed === true) {
+      res.render('you_win');
+      req.session.destroy();
+    } else {
+      res.render("start_game", {newWord: newWord});
+    }
   } else if (newWord[index].remainingGuesses === 1) { //when someone enters an incorrect guess, and the remaining guesses is one, we want to create an empty object!
     newWord[index].remainingGuesses = "";
     let clientGuess = {
       singleGuess: req.body.guess
     }
     newWord.push(clientGuess);
-    res.redirect("/");
-
+    res.render("end_Game");
+    req.session.destroy();
   } else if (!hiddenWord.includes(guess)) {
 newWord[index].remainingGuesses = newWord[index].remainingGuesses - 1;
 // can also write this as:
